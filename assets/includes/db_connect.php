90a302f3dc87fb59ba711587823a8f92aed3cbc6
Aaron Saleh <101801888+saaaron@users.noreply.github.com>
2024-08-15 12:30:50 +0100

    Update db_connect.php
    Remove unnecessary code lines

--- assets/includes/db_connect.php
+++ assets/includes/db_connect.php
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 <?php
 	$db = mysqli_connect('localhost', 'root', '', 'zipupload');
 
@@ -12,19 +11,4 @@
 	$project_name = "Zipupload"; // project's name
 	$description = "Easy file sharing"; // project's description
 	$author = "Sa Aaron"; // project's author
-=======
-<?php
-	$db = mysqli_connect('localhost', 'root', '', 'zipupload');
-
-	// Evaluate connection
-	if(mysqli_connect_errno()) {
-		echo 'Ops! A problem occured while trying to connect to database';
-		exit();
-	}
-
-	// project info
-	$project_name = "Zipupload"; // project's name
-	$description = "Easy file sharing"; // project's description
-	$author = "Sa Aaron"; // project's author
->>>>>>> 21342260079f1322b7c17c83ee9065cac944cdca
 ?>
